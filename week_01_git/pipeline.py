885f8f9af66edd295d920bcc317533688230f8ce
Mostafa Mohamed <mostafamohamed4.mm@gmail.com>
2026-09-03 17:53:55 +0300

    add load_data function

--- week_01_git/pipeline.py
+++ week_01_git/pipeline.py
@@ -2,9 +2,8 @@ def extract_data():
     print("Extracting data from API...")
 
 
-def transform_data():
-    print("Transforming data...")
-
+def load_data():
+    print("Loading data to PostgreSQL...")
 
 extract_data()
-transform_data()
+load_data()
