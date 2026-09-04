e93311b90c73e66ebf3e5b6d9140ec2623dd0084
Mostafa Mohamed <mostafamohamed4.mm@gmail.com>
2026-09-04 14:56:10 +0300

    add validate_data

--- week_01_git/pipeline.py
+++ week_01_git/pipeline.py
@@ -5,5 +5,11 @@ def extract_data():
 def load_data():
     print("Loading data to PostgreSQL...")
 
+
+def validate_data():
+    print("Validating data...")
+
+
 extract_data()
 load_data()
+validate_data()
